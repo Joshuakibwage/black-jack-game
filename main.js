07f8fc00c1f4915ca3df6293afe765bfc74395a5
Josh <joshuakibwage23@gmail.com>
2024-11-08 13:04:27 +0300

    Add function for genrating random numbers

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,9 +10,23 @@ let messageEl = document.getElementById('message-el');
 let sumEl = document.querySelector('#sum-el');
 let cardsEl = document.getElementById('cards-el');
 
-function startGame(){
+function getRandomCard(){
+
+  return Math.floor(Math.random() * 12) + 1;
+}
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame(){
   
-    cardsEl.textContent = "cards: " + firstCard + ' ' + secondCard ;
+    cardsEl.textContent = "cards: " ;
+
+    for(let i = 0; i < cards.length; i++){
+      cardsEl.textContent += cards[i] + " ";
+    }
+
     sumEl.textContent = "sum: " + sum;
     if(sum <= 20) {
         message = "Do you want to draw new card!"
@@ -26,5 +41,13 @@ function startGame(){
     messageEl.textContent = message;
 }
 
+function newCard() {
+  // 3. Use the getRandomCard() to set the value of card
+  let card = getRandomCard()
+  sum += card
+  cards.push(card)
+  console.log(cards)
+  renderGame()
+}
 
  

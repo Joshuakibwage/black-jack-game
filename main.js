09406cc70ac62e41c4ffd3eeed7b3ad3a7a1ae71
Josh <joshuakibwage23@gmail.com>
2024-11-10 09:58:55 +0300

    Add player element

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+ let player = {
+
+  name: "josh",
+  chips: 200
+ }
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById('message-el');
 let sumEl = document.querySelector('#sum-el');
 let cardsEl = document.getElementById('cards-el');
+let playerEl = document.getElementById('player-el');
+
+// let player = {
+//   name: "josh",
+//   chips: 145
+// }
+
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+// console.log(cards)
 
 function getRandomCard(){
 
-  return Math.floor(Math.random() * 12) + 1;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if(randomNumber > 10){
+    return 10
+  }else if(randomNumber === 1) {
+    return 11
+  }else{
+    return randomNumber
+  }
+
 }
 
 function startGame() {
+
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
     renderGame()
 }
 
@@ -41,13 +69,18 @@ function renderGame(){
     messageEl.textContent = message;
 }
 
+
 function newCard() {
   // 3. Use the getRandomCard() to set the value of card
-  let card = getRandomCard()
-  sum += card
-  cards.push(card)
-  console.log(cards)
-  renderGame()
+  if(isAlive === true && hasBlackJack === false){
+
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+  }
+
 }
 
  
